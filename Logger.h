5a1e210de575658529e7a2ba31257faab4da09f6
Dash <tsipluhina.dashenka@yandex.ru>
2025-08-24 14:32:13 +0300

    change Logger.h

--- Logger.h
+++ Logger.h
@@ -1,69 +1,39 @@
-#include "Logger.h"
-#include <stdexcept>
-#include <iomanip>
+#pragma once
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <ctime>
 #include <mutex>
 
-Logger::Logger(const std::string& file, LogLevel level) : defaultLevel(level) {
-    loggerFile.open(file, std::ios::app);
-    if (!loggerFile) {
-        throw std::runtime_error("Cannot open file: " + file);
-    }
-}
+// levels of significance in increasing importance
+enum class LogLevel {
+    Info = 0,
+    Warning = 1,
+    Error = 2
+};
 
-void Logger::setDefaultLevel(LogLevel level) {
-    std::lock_guard<std::mutex> lock(logMutex);
-    defaultLevel = level;
-}
+// class Logger to realize recordings
+class Logger {
+private:
+    std::ofstream loggerFile; // file for recordings
+    LogLevel defaultLevel;
+    std::mutex logMutex; // mutex for thread safety
 
-std::string Logger::getCurrentTime() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+public:
+    Logger(const std::string& file, LogLevel level = LogLevel::Info);
+    ~Logger();
 
-    std::tm timeinfo;
-#ifdef _WIN32
-    localtime_s(&timeinfo, &now_time);
-#else
-    localtime_r(&now_time, &timeinfo);
-#endif
+    // method to change default level
+    void setDefaultLevel(LogLevel level);
 
-    char timeBuffer[20];
-    std::strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d %H:%M:%S", &timeinfo);
-    return std::string(timeBuffer);
-}
+    // helpful method for recording to get string of current time
+    std::string getCurrentTime();
+    // from log level to string
+    std::string levelToString(LogLevel level);
+    //recording method
+    void recording(const std::string& message, LogLevel level);
 
-std::string Logger::levelToString(LogLevel level) {
-    switch (level) {
-    case LogLevel::Info:
-        return "Info: ";
-    case LogLevel::Warning:
-        return "Warning: ";
-    case LogLevel::Error:
-        return "Error: ";
-    }
-}
-
-void Logger::recording(const std::string& message, LogLevel level) {
-    std::lock_guard<std::mutex> lock(logMutex);
-
-    // check if we need to record this message
-    if (static_cast<int>(level) >= static_cast<int>(defaultLevel)) {
-
-        // record time
-        loggerFile << "[" << getCurrentTime() << "] ";
-
-        //record level
-        loggerFile << levelToString(level);
-
-        //record message
-        loggerFile << message << std::endl;
-        loggerFile.flush();
-    }
-}
-
-
-Logger::~Logger() {
-    std::lock_guard<std::mutex> lock(logMutex);
-    if (loggerFile.is_open()) {
-        loggerFile.close();
-    }
-}
+    // delete copy and assignment
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+};
